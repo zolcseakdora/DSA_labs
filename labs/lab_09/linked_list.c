ed601ff316368bbd18aa6554f9d2673b827a93a7
zolcseakdora <zolcseak.dora@student.ms.sapientia.ro>
2025-05-07 18:08:58 +0300

    9-s labor

---
+++ labs/lab_09/linked_list.c
@@ -0,0 +1,192 @@
+//
+// Created by Dora on 2025. 04. 17..
+//
+
+#include "linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Node *newNode(int new_data) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    if(!new_node) {
+        exit(-1);
+    }
+    new_node->data = new_data;
+    new_node->next = NULL;
+    return new_node;
+}
+bool isEmpty(Node *head) {
+    return head == NULL;
+}
+void insertAtBeginning(Node** head_ref, int new_data) {
+    // Allocate memory to a node
+    Node* new_node = newNode(new_data);
+    new_node->next = (*head_ref);
+    // Move head to new node
+    (*head_ref) = new_node;
+}
+void insertAfter(Node* prev_node, int new_data) {
+    if (prev_node == NULL) {
+        printf("NULL_POINTER_EXCEPTION_ERROR_MESSAGE");
+        return;
+        }
+    Node* new_node = newNode(new_data);
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
+}
+void insertAtEnd(Node** head_ref, int new_data) {
+    Node* new_node = newNode(new_data);
+    Node* last = *head_ref;
+    if (isEmpty(*head_ref)) {
+        *head_ref = new_node;
+        return;
+    }
+    while (last->next != NULL) last = last->next;
+    last->next = new_node;
+}
+void deleteFromBeginning(Node **head_ref) {
+    if(isEmpty(*head_ref)) {
+        printf("NULL_POINTER_EXCEPTION_ERROR_MESSAGE");
+        return;
+    }
+    Node *temp = *head_ref;
+    *head_ref = (*head_ref)->next;
+    free(temp);
+}
+void deleteFromEnd(Node **head_ref) {
+    if(isEmpty(*head_ref)) {
+        printf("NULL_POINTER_EXCEPTION_ERROR_MESSAGE");
+        return;
+    }
+    Node* last = *head_ref, *prev;
+    while (last->next != NULL)
+    {
+        prev = last;
+        last = last->next;
+    }
+    prev->next = NULL;
+    free(last);
+}
+void deleteNode(Node** head_ref,int key)
+{
+    Node *temp=*head_ref,*prev;
+    if (temp!=NULL && temp->data==key)
+    {
+        *head_ref=temp->next;
+        free(temp);
+        return;
+    }
+    while (temp!=NULL && temp->data != key)
+    {
+        prev=temp;
+        temp=temp->next;
+    }
+    if (temp==NULL) return;
+    prev->next =temp->next;
+    free(temp);
+}
+bool searchNode(Node* head_ref, int key) {
+    Node* current = head_ref;
+    while (current != NULL) {
+        if (current->data == key) return true;
+        current = current->next;
+    }
+    return false;
+}
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%4d ", node->data);
+        node = node->next;
+    }
+}
+void sortLinkedList(Node* head_ref) {
+    Node *p = (head_ref);
+    Node *q;
+    while (p->next != NULL) {
+        q = p->next;
+        while (q != NULL) {
+            if (p->data > q->data) {
+                int aux = p->data;
+                p->data = q->data;
+                q->data = aux;
+            }
+            q = q->next;
+        }
+        p = p->next;
+    }
+}
+void orderedList(Node **head_ref, int new_data) {
+    Node *prev=NULL, *last=NULL;
+    Node *node = newNode(new_data);
+    if (isEmpty(*head_ref)) {
+        (*head_ref) = node;
+        return;
+    } else {
+        if ((*head_ref)->data >= new_data) {
+        insertAtBeginning(head_ref, new_data);
+        return;
+        }
+    }
+    last = (*head_ref);
+    prev = NULL;
+    while (last != NULL && last->data <= new_data) {
+        prev = last;
+        last = last->next;
+    }
+    node->next = last;
+    if (prev != NULL) {
+        prev->next = node;
+    }
+}
+int getDigitList(int number, Node **head) {
+    // Szám számjegyeinek beillesztése hátulról
+    while (number > 0) {
+        int digit = number % 10;
+        insertAtEnd(head, digit);
+        number /= 10;
+    }
+    return 0;
+}
+
+void addDigitLists(Node* head1, Node* head2, Node **result) {
+    int carry = 0;
+    while (head1 != NULL || head2 != NULL || carry) {
+        int sum = carry;
+        if (head1 != NULL) {
+            sum += head1->data;
+            head1 = head1->next;
+        }
+        if (head2 != NULL) {
+            sum += head2->data;
+            head2 = head2->next;
+        }
+        insertAtEnd(result, sum % 10);
+        carry = sum / 10;
+    }
+}
+
+int listToInteger(Node* head) {
+    int result = 0;
+    int multiplier = 1;
+    while (head != NULL) {
+        result += head->data * multiplier;
+        multiplier *= 10;
+        head = head->next;
+    }
+    return result;
+}
+
+void insertZeroAfterEven(Node **head) {
+    Node* current = *head;
+    while (current != NULL) {
+        if (current->data % 2 == 0) {
+            Node* new_zero = newNode(0);
+            new_zero->next = current->next;
+            current->next = new_zero;
+            current = new_zero->next;
+        } else {
+            current = current->next;
+        }
+    }
+}
+
--- labs/lab_09/main.c
+++ labs/lab_09/main.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
+#include "linked_list.h"
 
 int main(void) {
-    printf("Hello, World!\n");
+    int num1, num2;
+    Node *list1 = NULL, *list2 = NULL, *sumList = NULL;
+
+    printf("Adj meg ket szamot: ");
+    scanf("%d %d", &num1, &num2);
+
+    getDigitList(num1, &list1);
+    getDigitList(num2, &list2);
+
+    printf("1. szam lista: ");
+    printList(list1);
+    printf("\n2. szam lista: ");
+    printList(list2);
+
+    addDigitLists(list1, list2, &sumList);
+    printf("\nOsszeg lista: ");
+    printList(sumList);
+
+    int sumValue = listToInteger(sumList);
+    printf("\nOsszeg ertek (szamkent): %d\n", sumValue);
+
+    insertZeroAfterEven(&sumList);
+    printf("\nParos szamjegyek utan 0 beszurasa: ");
+    printList(sumList);
+
+    insertAtBeginning(&sumList, 1);
+    insertAtEnd(&sumList, 1);
+    printf("\n1 beszurasa az elejere es vegere: ");
+    printList(sumList);
+
+    int toDelete;
+    printf("\nAdj meg egy szamot, amit torolni szeretnel a listabol: ");
+    scanf("%d", &toDelete);
+
+    if (searchNode(sumList, toDelete)) {
+        deleteNode(&sumList, toDelete);
+        printf("Torles utan: ");
+        printList(sumList);
+    } else {
+        printf("A megadott szam nincs a listaban.\n");
+    }
+
+    sortLinkedList(sumList);
+    printf("\nRendezett lista novekvo sorrendben: ");
+    printList(sumList);
+    printf("\n");
+
     return 0;
 }
